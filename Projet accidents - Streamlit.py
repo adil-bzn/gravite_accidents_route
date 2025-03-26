da87b1b9bea443c1cfcba4204a9a82a4a75d792f
Kaalinodi57 <Kaalinodi@gmail.com>
2025-03-26 22:11:16 +0100

    Code Streamlit

--- Projet accidents - Streamlit.py
+++ Projet accidents - Streamlit.py
@@ -16,7 +16,7 @@ caracteristiques = r"C:\Users\adilb\OneDrive\Documents\Projets\Formation Data An
 lieux = r"C:\Users\adilb\OneDrive\Documents\Projets\Formation Data Analyst\Projet\Cohorte août 24 - Projet accidents\0 - Jeux de données\lieux.csv"
 usagers = r"C:\Users\adilb\OneDrive\Documents\Projets\Formation Data Analyst\Projet\Cohorte août 24 - Projet accidents\0 - Jeux de données\usagers.csv"
 vehicules = r"C:\Users\adilb\OneDrive\Documents\Projets\Formation Data Analyst\Projet\Cohorte août 24 - Projet accidents\0 - Jeux de données\vehicules.csv"
-description_variables =r"C:\Users\adilb\Downloads\Projets accidents - Description des variables.xlsx"
+description_variables = r"C:\Users\adilb\Downloads\Projets accidents - Description des variables.xlsx"
 df_caracteristiques = pd.read_csv(caracteristiques, encoding = "ISO-8859-1", index_col=0)
 df_lieux = pd.read_csv(lieux, encoding = "ISO-8859-1",index_col=0)
 df_usagers = pd.read_csv(usagers, encoding = "ISO-8859-1",index_col=0)
@@ -38,6 +38,7 @@ st.sidebar.write("Malika Sadki")
 st.sidebar.write("Anouar Ouro-Sao")
 
 if page == pages[1] :
+    st.write("\n\n\n\n\n")
 
     st.write("Description du BAAC")
     st.write("Description de la variable gravité")
@@ -57,14 +58,56 @@ if page == pages[1] :
     if checkbox_df_selected == "df_vehicules" :
         st.dataframe(df_vehicules.head(5))
 
-    #A FAIRE : Mettre l'image de la jointure entre les tables
-
     st.write("Explication sur le choix de supprimer les années >= à 2018")
-    st.write("Relever la présence de d oublons dans le df_usagers")
-
+    st.write("Relever la présence de doublon dans le df_usagers")
+    
     df_usagers=df_usagers.drop_duplicates(keep="first")
+
+
+    st.image(
+            "https://raw.githubusercontent.com/Kaalinodi57/accidents-routes-cda/refs/heads/main/Tables%20et%20jointures.png",width=400)
+    st.write("Expliquer la méthodologie de jointure")
 
     st.write("Mise à disposition du dictionnaire de chaque variable")
     st.dataframe(df_description_variables, hide_index=True)
 
 if page == pages[2] :
+    st.write("\n\n\n\n\n\n\n\n\n\n")
+
+    #Titre 1
+    st.write("Cartographie des accidents en France Metropolitaine")
+    st.write("\n\n")
+    st.image("https://raw.githubusercontent.com/Kaalinodi57/accidents-routes-cda/refs/heads/main/Image%20Visualisation/Cartographie%20du%20nombre%20d'accident%20en%20France%20M%C3%A9tropolitaine.png")
+    #A FAIRE : Bullet point
+    #A FAIRE : Explication détaillée
+
+    st.write("\n\n\n\n\n")
+    #Titre 1
+    st.write("Cartographie des accidents en France Metropolitaine en fonction de la gravité",caption="Répartition du nombre d'accident en France Métropolitaine entre 2005 et 2017.")
+    st.write("\n\n")
+
+    checkbox_CarteGravite_selected = st.selectbox("Afficher la répartition d'une des 4 gravités sur la France Métropolitaine :", ("1 - Indemne","2 - Tué","3 - Blessé grave","4 - Blessé léger"), index=None, placeholder="Sélectionner une gravité")
+    if  checkbox_CarteGravite_selected == "1 - Indemne" :
+        st.image("https://raw.githubusercontent.com/Kaalinodi57/accidents-routes-cda/refs/heads/main/Image%20Visualisation/Cartographie%20nombre%20accident%20et%20nombre%20accident%20par%20gravit%C3%A9%20indemne%20en%20France%20M%C3%A9tropolitaine.png",caption="Répartition du nombre d'accident (violet) et du nombre d'accident par gravité en France Métropolitaine entre 2005 et 2017.")
+
+    if checkbox_CarteGravite_selected == "2 - Tué" :
+        st.image("https://raw.githubusercontent.com/Kaalinodi57/accidents-routes-cda/refs/heads/main/Image%20Visualisation/Cartographie%20nombre%20accident%20et%20nombre%20accident%20par%20gravit%C3%A9%20tu%C3%A9s%20en%20France%20M%C3%A9tropolitaine.png",caption="Répartition du nombre d'accident (violet) et du nombre d'accident par gravité en France Métropolitaine entre 2005 et 2017.")
+
+    if checkbox_CarteGravite_selected == "3 - Blessé grave" :
+        st.image("https://raw.githubusercontent.com/Kaalinodi57/accidents-routes-cda/refs/heads/main/Image%20Visualisation/Cartographie%20nombre%20accident%20et%20nombre%20accident%20par%20gravit%C3%A9%20bless%C3%A9s%20graves%20en%20France%20M%C3%A9tropolitaine.png",caption="Répartition du nombre d'accident (violet) et du nombre d'accident par gravité en France Métropolitaine entre 2005 et 2017.")
+
+    if checkbox_CarteGravite_selected == "4 - Blessé léger" :
+        st.image("https://raw.githubusercontent.com/Kaalinodi57/accidents-routes-cda/refs/heads/main/Image%20Visualisation/Cartographie%20nombre%20accident%20et%20nombre%20accident%20par%20gravit%C3%A9%20bless%C3%A9s%20l%C3%A9gers%20en%20France%20M%C3%A9tropolitaine.png",caption="Répartition du nombre d'accident (violet) et du nombre d'accident par gravité en France Métropolitaine entre 2005 et 2017.")
+    #A FAIRE : Bullet point
+    #A FAIRE : Explication détaillée
+
+    st.write("\n\n\n\n\n")
+    #Titre 1
+    st.write("Accidents par département")
+    st.write("\n\n")
+    st.image("https://raw.githubusercontent.com/Kaalinodi57/accidents-routes-cda/refs/heads/main/Image%20Visualisation/20%20d%C3%A9partements%20avec%20le%20plus%20d'accident.png", width = 600, caption="Les 20 départements avec le plus d'accident entre 2005 et 2017.")
+    st.write("\n")
+    st.image("https://raw.githubusercontent.com/Kaalinodi57/accidents-routes-cda/refs/heads/main/Image%20Visualisation/20%20d%C3%A9partements%20avec%20le%20moins%20d'accident.png", width = 600, caption="Les 20 départements avec le moins d'accident entre 2005 et 2017.")
+    #A FAIRE : Bullet point
+    #A FAIRE : Explication détaillée
+
